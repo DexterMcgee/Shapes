176405cea9664f7156da6fea17e645226ed662fc
victor <victorzemail@gmail.com>
2022-06-20 19:42:31 -0500

    Shapes with Themes

---
+++ script.js
@@ -0,0 +1,287 @@
+jQuery(function () {
+  $(shapes.shape1).on("click", function () {
+    if ($(this).hasClass("shape-1")) {
+      $(this).removeClass("shape-1").addClass("alt-theme-1-shape-1");
+    } else if ($(this).hasClass("alt-theme-1-shape-1")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-1")
+        .addClass("alt-theme-2-shape-1");
+    } else if ($(this).hasClass("alt-theme-2-shape-1")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-1")
+        .addClass("alt-theme-3-shape-1");
+    } else if ($(this).hasClass("alt-theme-3-shape-1")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-1")
+        .addClass("alt-theme-4-shape-1");
+    } else if ($(this).hasClass("alt-theme-4-shape-1")) {
+      $(this).removeClass("alt-theme-4-shape-1").addClass("shape-1");
+    }
+  });
+  $(shapes.shape2).on("click", function () {
+    if ($(this).hasClass("shape-2")) {
+      $(this).removeClass("shape-2").addClass("alt-theme-1-shape-2");
+    } else if ($(this).hasClass("alt-theme-1-shape-2")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-2")
+        .addClass("alt-theme-2-shape-2");
+    } else if ($(this).hasClass("alt-theme-2-shape-2")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-2")
+        .addClass("alt-theme-3-shape-2");
+    } else if ($(this).hasClass("alt-theme-3-shape-2")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-2")
+        .addClass("alt-theme-4-shape-2");
+    } else if ($(this).hasClass("alt-theme-4-shape-2")) {
+      $(this).removeClass("alt-theme-4-shape-2").addClass("shape-2");
+    }
+  });
+  $(shapes.shape3).on("click", function () {
+    if ($(this).hasClass("shape-3")) {
+      $(this).removeClass("shape-3").addClass("alt-theme-1-shape-3");
+    } else if ($(this).hasClass("alt-theme-1-shape-3")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-3")
+        .addClass("alt-theme-2-shape-3");
+    } else if ($(this).hasClass("alt-theme-2-shape-3")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-3")
+        .addClass("alt-theme-3-shape-3");
+    } else if ($(this).hasClass("alt-theme-3-shape-3")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-3")
+        .addClass("alt-theme-4-shape-3");
+    } else if ($(this).hasClass("alt-theme-4-shape-3")) {
+      $(this).removeClass("alt-theme-4-shape-3").addClass("shape-3");
+    }
+  });
+  $(shapes.shape4).on("click", function () {
+    if ($(this).hasClass("shape-4")) {
+      $(this).removeClass("shape-4").addClass("alt-theme-1-shape-4");
+    } else if ($(this).hasClass("alt-theme-1-shape-4")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-4")
+        .addClass("alt-theme-2-shape-4");
+    } else if ($(this).hasClass("alt-theme-2-shape-4")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-4")
+        .addClass("alt-theme-3-shape-4");
+    } else if ($(this).hasClass("alt-theme-3-shape-4")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-4")
+        .addClass("alt-theme-4-shape-4");
+    } else if ($(this).hasClass("alt-theme-4-shape-4")) {
+      $(this).removeClass("alt-theme-4-shape-4").addClass("shape-4");
+    }
+  });
+  $(shapes.shape5).on("click", function () {
+    if ($(this).hasClass("shape-5")) {
+      $(this).removeClass("shape-5").addClass("alt-theme-1-shape-5");
+    } else if ($(this).hasClass("alt-theme-1-shape-5")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-5")
+        .addClass("alt-theme-2-shape-5");
+    } else if ($(this).hasClass("alt-theme-2-shape-5")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-5")
+        .addClass("alt-theme-3-shape-5");
+    } else if ($(this).hasClass("alt-theme-3-shape-5")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-5")
+        .addClass("alt-theme-4-shape-5");
+    } else if ($(this).hasClass("alt-theme-4-shape-5")) {
+      $(this).removeClass("alt-theme-4-shape-5").addClass("shape-5");
+    }
+  });
+  $(shapes.shape6).on("click", function () {
+    if ($(this).hasClass("shape-6")) {
+      $(this).removeClass("shape-6").addClass("alt-theme-1-shape-6");
+    } else if ($(this).hasClass("alt-theme-1-shape-6")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-6")
+        .addClass("alt-theme-2-shape-6");
+    } else if ($(this).hasClass("alt-theme-2-shape-6")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-6")
+        .addClass("alt-theme-3-shape-6");
+    } else if ($(this).hasClass("alt-theme-3-shape-6")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-6")
+        .addClass("alt-theme-4-shape-6");
+    } else if ($(this).hasClass("alt-theme-4-shape-6")) {
+      $(this).removeClass("alt-theme-4-shape-6").addClass("shape-6");
+    }
+  });
+  $(shapes.shape7).on("click", function () {
+    if ($(this).hasClass("shape-7")) {
+      $(this).removeClass("shape-7").addClass("alt-theme-1-shape-7");
+    } else if ($(this).hasClass("alt-theme-1-shape-7")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-7")
+        .addClass("alt-theme-2-shape-7");
+    } else if ($(this).hasClass("alt-theme-2-shape-7")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-7")
+        .addClass("alt-theme-3-shape-7");
+    } else if ($(this).hasClass("alt-theme-3-shape-7")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-7")
+        .addClass("alt-theme-4-shape-7");
+    } else if ($(this).hasClass("alt-theme-4-shape-7")) {
+      $(this).removeClass("alt-theme-4-shape-7").addClass("shape-7");
+    }
+  });
+  $(shapes.shape8).on("click", function () {
+    if ($(this).hasClass("shape-8")) {
+      $(this).removeClass("shape-8").addClass("alt-theme-1-shape-8");
+    } else if ($(this).hasClass("alt-theme-1-shape-8")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-8")
+        .addClass("alt-theme-2-shape-8");
+    } else if ($(this).hasClass("alt-theme-2-shape-8")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-8")
+        .addClass("alt-theme-3-shape-8");
+    } else if ($(this).hasClass("alt-theme-3-shape-8")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-8")
+        .addClass("alt-theme-4-shape-8");
+    } else if ($(this).hasClass("alt-theme-4-shape-8")) {
+      $(this).removeClass("alt-theme-4-shape-8").addClass("shape-8");
+    }
+  });
+  $(shapes.shape9).on("click", function () {
+    if ($(this).hasClass("shape-9")) {
+      $(this).removeClass("shape-9").addClass("alt-theme-1-shape-9");
+    } else if ($(this).hasClass("alt-theme-1-shape-9")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-9")
+        .addClass("alt-theme-2-shape-9");
+    } else if ($(this).hasClass("alt-theme-2-shape-9")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-9")
+        .addClass("alt-theme-3-shape-9");
+    } else if ($(this).hasClass("alt-theme-3-shape-9")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-9")
+        .addClass("alt-theme-4-shape-9");
+    } else if ($(this).hasClass("alt-theme-4-shape-9")) {
+      $(this).removeClass("alt-theme-4-shape-9").addClass("shape-9");
+    }
+  });
+  $(shapes.shape10).on("click", function () {
+    if ($(this).hasClass("shape-10")) {
+      $(this).removeClass("shape-10").addClass("alt-theme-1-shape-10");
+    } else if ($(this).hasClass("alt-theme-1-shape-10")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-10")
+        .addClass("alt-theme-2-shape-10");
+    } else if ($(this).hasClass("alt-theme-2-shape-10")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-10")
+        .addClass("alt-theme-3-shape-10");
+    } else if ($(this).hasClass("alt-theme-3-shape-10")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-10")
+        .addClass("alt-theme-4-shape-10");
+    } else if ($(this).hasClass("alt-theme-4-shape-10")) {
+      $(this).removeClass("alt-theme-4-shape-10").addClass("shape-10");
+    }
+  });
+  $(shapes.shape11).on("click", function () {
+    if ($(this).hasClass("shape-11")) {
+      $(this).removeClass("shape-11").addClass("alt-theme-1-shape-11");
+    } else if ($(this).hasClass("alt-theme-1-shape-11")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-11")
+        .addClass("alt-theme-2-shape-11");
+    } else if ($(this).hasClass("alt-theme-2-shape-11")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-11")
+        .addClass("alt-theme-3-shape-11");
+    } else if ($(this).hasClass("alt-theme-3-shape-11")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-11")
+        .addClass("alt-theme-4-shape-11");
+    } else if ($(this).hasClass("alt-theme-4-shape-11")) {
+      $(this).removeClass("alt-theme-4-shape-11").addClass("shape-11");
+    }
+  });
+  $(shapes.shape12).on("click", function () {
+    if ($(this).hasClass("shape-12")) {
+      $(this).removeClass("shape-12").addClass("alt-theme-1-shape-12");
+    } else if ($(this).hasClass("alt-theme-1-shape-12")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-12")
+        .addClass("alt-theme-2-shape-12");
+    } else if ($(this).hasClass("alt-theme-2-shape-12")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-12")
+        .addClass("alt-theme-3-shape-12");
+    } else if ($(this).hasClass("alt-theme-3-shape-12")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-12")
+        .addClass("alt-theme-4-shape-12");
+    } else if ($(this).hasClass("alt-theme-4-shape-12")) {
+      $(this).removeClass("alt-theme-4-shape-12").addClass("shape-12");
+    }
+  });
+  $(shapes.shape13).on("click", function () {
+    if ($(this).hasClass("shape-13")) {
+      $(this).removeClass("shape-13").addClass("alt-theme-1-shape-13");
+    } else if ($(this).hasClass("alt-theme-1-shape-13")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-13")
+        .addClass("alt-theme-2-shape-13");
+    } else if ($(this).hasClass("alt-theme-2-shape-13")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-13")
+        .addClass("alt-theme-3-shape-13");
+    } else if ($(this).hasClass("alt-theme-3-shape-13")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-13")
+        .addClass("alt-theme-4-shape-13");
+    } else if ($(this).hasClass("alt-theme-4-shape-13")) {
+      $(this).removeClass("alt-theme-4-shape-13").addClass("shape-13");
+    }
+  });
+  $(shapes.shape14).on("click", function () {
+    if ($(this).hasClass("shape-14")) {
+      $(this).removeClass("shape-14").addClass("alt-theme-1-shape-14");
+    } else if ($(this).hasClass("alt-theme-1-shape-14")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-14")
+        .addClass("alt-theme-2-shape-14");
+    } else if ($(this).hasClass("alt-theme-2-shape-14")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-14")
+        .addClass("alt-theme-3-shape-14");
+    } else if ($(this).hasClass("alt-theme-3-shape-14")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-14")
+        .addClass("alt-theme-4-shape-14");
+    } else if ($(this).hasClass("alt-theme-4-shape-14")) {
+      $(this).removeClass("alt-theme-4-shape-14").addClass("shape-14");
+    }
+  });
+  $(shapes.shape15).on("click", function () {
+    if ($(this).hasClass("shape-15")) {
+      $(this).removeClass("shape-15").addClass("alt-theme-1-shape-15");
+    } else if ($(this).hasClass("alt-theme-1-shape-15")) {
+      $(this)
+        .removeClass("alt-theme-1-shape-15")
+        .addClass("alt-theme-2-shape-15");
+    } else if ($(this).hasClass("alt-theme-2-shape-15")) {
+      $(this)
+        .removeClass("alt-theme-2-shape-15")
+        .addClass("alt-theme-3-shape-15");
+    } else if ($(this).hasClass("alt-theme-3-shape-15")) {
+      $(this)
+        .removeClass("alt-theme-3-shape-15")
+        .addClass("alt-theme-4-shape-15");
+    } else if ($(this).hasClass("alt-theme-4-shape-15")) {
+      $(this).removeClass("alt-theme-4-shape-15").addClass("shape-15");
+    }
+  });
+});
